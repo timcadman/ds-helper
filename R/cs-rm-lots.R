85e6aa47a9cd7876cc2863f38a6e736d4295d8a5
timcadman <tim@molgenis89.gcc.rug.nl>
2020-07-01 17:23:19 +0200

    Put into package format

--- R/cs-rm-lots.R
+++ R/cs-rm-lots.R
@@ -1,23 +1,17 @@
-################################################################################
-## Project: ds-cs-functions
-## Script purpose: Allow use of ds.rm with more than one variable
-## Date: 5th June 2020
-## Author: Tim Cadman
-## Email: t.cadman@bristol.ac.uk
-################################################################################        
-
-# This is a very simple wrapper around ds.rm to allow you to remove more than
-# one object in one function call.
-#
-# Arguments:
-#
-# vars = objects that you want to remove
-#
-# Value: None. Objects remove from opal environment
-
-require(purrr)
-require(dplyr)
-
+#' Remove multiple objects from ds environment
+#' 
+#' This is a very simple wrapper around ds.rm to allow you to remove more than 
+#' one object at a time.
+#' 
+#' @param vars objects that you want to remove
+#' 
+#' @return None. Objects removed from ds environment
+#' 
+#' @importFrom purrr map
+#' @importFrom dsBaseClient ds.rm
+#' @importFrom dplyr %>%
+#' 
+#' @export
 cs.rmLots <- function(vars){
   
   vars %>% map(ds.rm)
