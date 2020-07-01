00a334b42cfddd23e421b26237ca1b7a2b375104
timcadman <tim@molgenis89.gcc.rug.nl>
2020-07-01 16:38:21 +0200

    Put into package format, added check at beginning of function

--- R/cs-find-vars-index.R
+++ R/cs-find-vars-index.R
@@ -1,30 +1,34 @@
-################################################################################
-## Project: ds-cs-functions 
-## Script purpose: Return index of variables  
-## Date: 5th June 2020
-## Author: Tim Cadman
-## Email: t.cadman@bristol.ac.uk
-################################################################################
-
-# Some ds functions ask you to provide the index of variables as arguments.
-# This is highly susceptable to breaking! This is a simple function that will
-# return the index of variables for a given cohort. It is usefully used in 
-# combination the "keep.cols" argument of ds.dataFrameSubset.
-#
-# Arguments:
-# 
-# df = a dataframe
-# vars = vector of variables
-# cohorts = cohort that you want to find index for
-#
-# value = vector of indexes
-
-require(dsBaseClient)
-require(purrr)
-require(stringr)
-
+#' Return return indices of columns in opal dataframe
+#' 
+#' Some ds functions ask you to provide the index of variables as arguments.
+#' This is highly susceptable to breaking! This is a simple function that will
+#' return the index of variables for a given cohort. It is usefully used in 
+#' combination the "keep.cols" argument of ds.dataFrameSubset.
+#' 
+#' @param df opal dataframe
+#' @param vars vector of variable names in dataframe
+#' @param cohorts cohort that you want to find indices for
+#' 
+#' @return list of indices where length of list is number of cohorts provided
+#' 
+#' @importFrom dsBaseClient ds.colnames
+#' @importFrom stringr str_detect
+#' @importFrom purrr map_dbl
+#' 
+#' @export
 cs.findVarsIndex <- function(df, vars, cohorts){
   
+  varcheck <- cs.doVarsExist(df, vars)
+  
+  if(varcheck[[1]] == FALSE){
+    
+    stop(paste0(
+      "The following variable(s) are not present in the data frame: ", 
+      paste0(varcheck[[2]], collapse = ", ")), call. = FALSE
+    )
+    
+  } else{
+
    map_dbl(vars,
       
       ~which(
@@ -34,5 +38,7 @@ cs.findVarsIndex <- function(df, vars, cohorts){
                  .x,
                  "\\b")) == TRUE)
     )  
+  
+  }
   
 }
