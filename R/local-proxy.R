1f24c06693746154784d64a29ea0ae1e64ac2bc5
tombisho <trpb2@cam.ac.uk>
2020-12-02 05:52:21 -0800

    added new function

--- R/local-proxy.R
+++ R/local-proxy.R
@@ -1,20 +1,36 @@
-#' Check whether variables exist in dataframe
+#' Generate a local proxy dataframe to enable local autocompletion in RStudio
 #'
-#' Probably mostly for internal use, this function can be used at the start
-#' of functions where users provide both vars and a dataframe to check that
-#' the vars exist in the dataframe provided
+#' If you have a remote dataframe on the servers, it can be challenging to remember variable narmes when writing code.
+#' Because the dataframes are on remote servers, the autocomplete won't work in RStudio. The purpose of this function
+#' is to create a local object that has the same structure as the remote data frame and enables the use of autocomplete
+#' when writing DataSHIELD code.
+#' 
+#' For example, if you have a dataframe "D" with columns LAB_TSC, DIS_CVA and DIS_DIAB, normally you would need to write:
+#' 
+#' `ds.summary("D$LAB_TSC")` with the variable name spelled correctly (no autocomplete!) and remember quotation marks
+#' 
+#' after running this function you can type
+#' 
+#' `ds.summary(D$` and hit tab, and the list of variables will be displayed. Choose one and hit enter to finish up with:
+#' 
+#' `ds.summary(D$LAB_TSC)` - no quotation marks needed either!
 #'
 #' @param conns connections object to DataSHIELD backends
 #' @param df datashield dataframe
 #'
-#' @return None. Stops function if var(s) don't exist in one of more cohorts.
+#' @return Creates a local proxy dataframe. Stops function if df doesn't exist or is inconsistent in one of more cohorts.
 #'
-#' @importFrom purrr map
-#' @importFrom dsBaseClient ds.colnames
 #'
-#' @noRd
+#' @export
 dh.localProxy <- function(conns = conns, df) {
-  dh.doesDfExist(conns, df)
+  discrep = dh.classDiscrepancy(conns = conns, df = df)
+  if(any(discrep$discrepancy=="yes")){
+    warning("All columns not found in all cohorts, please see tibble returned and correct this", call. = FALSE)
+    return(discrep)
+  }
+  tempDF = data.frame(matrix(paste0("D$",discrep$variable), ncol = length(discrep$variable), nrow = 1))
+  colnames(tempDF) = discrep$variable
+  assign(df,tempDF, envir = parent.frame())
 
   
 }
