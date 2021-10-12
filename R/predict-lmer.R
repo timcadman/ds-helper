5f54f06887f912f7fc1c106024fc965c6656f2c9
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2021-10-12 15:23:10 +0200

    Now calculates standard errors for cohort estimates

--- R/predict-lmer.R
+++ R/predict-lmer.R
@@ -14,12 +14,12 @@ coefs <- dh.lmTab(
   ci_format = "separate") 
 
 ## ---- Now we get the names of coefficients which aren't the intercept --------
-coef_names <- coefs %>% 
+coef_names <- coefs$fixed %>% 
 pull(variable) %>%
 unique
 
 ## ---- Now we get the coefficients for each cohort ----------------------------
-coefs_by_cohort <- coefs %>%
+coefs_by_cohort <- coefs$fixed %>%
 pivot_wider(
 	names_from = variable,
   values_from = value) %>%
@@ -58,8 +58,64 @@ map(function(x){
 newdata %>%
 mutate(predicted = rowSums(x))
 
+}) 
+
+
+## ---- Now get the standard errors ------------------------------------------------
+nstudy <- seq(1, model$num.valid.studies, 1)
+study_ref <- paste0("study", nstudy)
+
+## We get the vcov matrix for each study
+vcov <- study_ref %>%
+map(~model$output.summary[[.]]$vcov)
+
+## Now we need to make sure our new data has the same order of columns as the
+## vcov. This is slightly annoying because we had renamed our intercept 
+## term to have a less silly name. We rename it again, get the columns in the 
+## right order then put it back.
+
+newdata <- newdata %>%
+dplyr::rename("(Intercept)" = intercept) %>%
+dplyr::select(colnames(vcov[[1]])) %>%
+dplyr::rename(intercept = "(Intercept)")
+
+## Feed in our newdata frame to get the SEs
+se <- vcov %>% map(function(x){
+
+newdata %>%
+  pmap_dbl(function(...){
+
+C <- c(...)
+std.er <- sqrt(t(C) %*% x %*% C)
+out <- std.er@x
+
+return(out)
+})
+
 }) %>%
+set_names(coh_names) %>%
+map(as_tibble)
+
+## Now currently we haven't found a way to compute standard errors for the pooled
+## results. So as a placeholder we will create a list the same length as for each
+## cohort and set that to NA.
+
+comb_se <- list(
+  combined = tibble(
+    value = rep(NA, nrow(se[[1]]))
+  )
+)
+
+se <- c(se, comb_se)
+
+## ---- Join the standard errors back in with the predicted values ----------------
+out <- names(pred) %>%
+map(~bind_cols(pred[[.x]], se[[.x]])) %>%
+map(~dplyr::rename(., se = value)) %>%
+map(~mutate(., low_ci = predicted - 1.96 * se, 
+         upper_ci = predicted + 1.96 * se)) %>%
+set_names(names(pred)) %>%
 bind_rows(.id = "cohort")
 
-return(pred)
+return(out)
 }
