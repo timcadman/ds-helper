5c9769d3f31b430f9ba382b4e65383f717511c68
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2021-10-13 08:35:51 +0200

    Added missing function from importfrom plus unbound variables

--- R/get-stats.R
+++ R/get-stats.R
@@ -45,7 +45,7 @@
 #' @importFrom tibble as_tibble tibble
 #' @importFrom dplyr %>% arrange group_by group_map summarise summarize ungroup
 #' left_join bind_rows rename filter mutate_at vars distinct add_row
-#' @importFrom purrr map flatten_dbl pmap
+#' @importFrom purrr map flatten_dbl pmap_chr
 #' @importFrom tidyr replace_na
 #' @importFrom dsBaseClient ds.length ds.dim ds.levels
 #' @importFrom stringr str_detect
@@ -82,7 +82,8 @@ dh.getStats <- function(df = NULL, vars = NULL, conns = NULL, digits = 2) {
     missing_n <- perc_missing <- EstimatedVar <- Nvalid <- any_obs <-
     bind_cols <- cohort <- combined <- disc <- discrepancy <- key_stats <-
     n_distinct <- out_cont <- outcome <- same_levels <- se <- stat <-
-    stats_tmp <- stats_wide <- std.dev <- type <- type_w_null <- . <- NULL
+    stats_tmp <- stats_wide <- std.dev <- type <- type_w_null <- . <- 
+    perc_valid <- perc_total <- Ntotal <- NULL
 
   ################################################################################
   # 2. Get classes of variables
