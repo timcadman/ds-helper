6e46a4997853252d2a511d8f00593bc447aa942a
Sido Haakma <sido@haakma.org>
2021-10-20 22:05:14 +0200

    chore: remove linters for now

--- .azure/pullrequest.bash
+++ .azure/pullrequest.bash
@@ -4,5 +4,5 @@ Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', usethis::use_version('$
 Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', devtools::install())"
 Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', devtools::check(force_suggests = TRUE))"
 #Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', covr::codecov())"
-Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', lintr::lint_package())"
-Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', quit(save = 'no', status = length(lintr::lint_package(linters=lintr::with_defaults(object_usage_linter = NULL)))))"
+#Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', lintr::lint_package(linters=lintr::with_defaults(object_usage_linter = NULL, lintr::line_length_linter(120))))"
+#Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', quit(save = 'no', status = length(lintr::lint_package(linters=lintr::with_defaults(object_usage_linter = 'dotted.case', line_length_linter(120))))))"
