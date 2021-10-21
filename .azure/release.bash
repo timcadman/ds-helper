5ad243914d375304b6f5b42eb6f6e2a4f764f308
Sido Haakma <sido@haakma.org>
2021-10-21 17:13:49 +0200

    chore: added github user to env of azure

--- .azure/release.bash
+++ .azure/release.bash
@@ -9,17 +9,17 @@
 # - SYSTEM_PULLREQUEST_TARGETBRANCH: PullRequest target branch e.g. main
 # Additional environment variables to make sure the release works
 # - GITHUB_TOKEN: password used to push to github
-# - GITHUB_USER: username used to push to github
 # - NEXUS_USER: repository username
 # - NEXUS_PASS: repository password
 # - REGISTRY: repository url
 # - R_LIBS_USER: home directory user libraries
 # - CODECOV_TOKEN: token to authenticate to codecov
 
-git remote set-url origin "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/lifecycle-project/ds-helper.git"
+git remote set-url origin "https://${GITHUB_TOKEN}@github.com/lifecycle-project/ds-helper.git"
+git checkout -f master
+git fetch --tags
 Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', git2r::config(user.email = 'sido@haakma.org', user.name = 'Azure Pipeline'))"
 RELEASE_SCOPE="patch"
-git checkout -f master
 Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', usethis::use_version('${RELEASE_SCOPE}'))"
 TAG=$(grep Version DESCRIPTION | head -n1 | cut -d':' -f2 | xargs)
 PACKAGE=$(grep Package DESCRIPTION | head -n1 | cut -d':' -f2 | xargs)
@@ -29,8 +29,7 @@ R CMD build .
 Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', devtools::check_built(path = './${PACKAGE}_${TAG}.tar.gz', force_suggests = TRUE))"
 set +x; curl -v --user "${NEXUS_USER}:${NEXUS_PASS}" --upload-file "${PACKAGE}_${TAG}".tar.gz "${REGISTRY}"/src/contrib/"${PACKAGE}_${TAG}".tar.gz
 git tag "${TAG}"
-git push origin "${TAG}"
 echo "Creating new development version for R-package: [ ${BUILD_REPOSITORY_NAME} ]"
 Rscript -e "withr::with_libpaths(new = '${R_LIBS_USER}', usethis::use_version('dev'))"
 git commit -a -m '[ci skip]: Increment dev-version number'
-git push origin master
+git push --tags origin master
