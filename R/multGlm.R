94f8c85376396db4588b5844fba956851916cb8c
timcadman <timcadman@gmail.com>
2024-04-04 15:03:56 +0000

    fix: removed erroneous weight_obj argument, moved functions back into try catch which seemed to fix it and stopped using dplyr to build a tibble which also fixed it. This is a big hack really but lets see what happens

--- R/multGlm.R
+++ R/multGlm.R
@@ -42,29 +42,38 @@ dh.multGLM <- function(df = NULL, ref = NULL, checks = TRUE, conns = NULL,
   ## ---- Run the models -------------------------------------------------------
   suppressWarnings(
     glm_out <- ref %>%
-      pmap(function(formula, cohort, weight_obj, ...) {
+      pmap(function(formula, cohort, ...) {
         
         tryCatch(
-          {multGlmWraper(formula, df, family, weights, cohort, conns)},
-          error = multHandleError(error_message)
+          {
+            ds.glmSLMA(
+            formula = unlist(formula),
+            dataName = df,
+            family = family,
+            weights = weights,
+            combine.with.metafor = TRUE,
+            datasources = conns[cohort])
+            },
+          error = function(error_message) {
+            out <- list("failed", error_message)
+            return(out)
+            }
         )
         
       })
   )
    
-formula = ref$formula
+# formula = ref$formula
 
   ## ---- Identify models which failed completely ------------------------------
   fail_tmp <- glm_out %>%
     map(~ .[[1]][[1]][[1]]) %>%
     str_detect("POTENTIALLY DISCLOSIVE|failed", negate = TRUE)
 
-  out <- ref %>%
-    mutate(
-      fit = glm_out,
-      converged = fail_tmp
-    )
-
+  out <- ref 
+  out$fit <- glm_out
+  out$converged <- fail_tmp
+  
   problems <- out %>%
     dplyr::filter(converged == FALSE)
 
@@ -96,17 +105,4 @@ multCheckArgs <- function(df, ref, vary_df){
 multHandleError <- function(error_message){
   out <- list("failed", error_message)
   return(out)
-}
-
-multGlmWraper <- function(formula, df, family, weights, cohort, conns){
-  
-  ds.glmSLMA(
-    formula = unlist(formula),
-    dataName = df,
-    family = family,
-    combine.with.metafor = TRUE,
-    weights = weights,
-    datasources = conns[cohort]
-  )
-  
 }
